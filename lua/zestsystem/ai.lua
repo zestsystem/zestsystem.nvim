24e6ab17f8e608bc89a81af0ae75af279ee597b5
zestsystem <mk337337@gmail.com>
2025-02-11 21:06:33 -0800

    fix: remove flags for AiderAddModifiedFiles

--- lua/zestsystem/ai.lua
+++ lua/zestsystem/ai.lua
@@ -17,7 +17,7 @@ local function init()
     -- Aider setup
     aider.setup {}
     vim.api.nvim_set_keymap('n', '<leader>Ao', ':AiderOpen --mini --no-auto-commits<CR>', { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<leader>Am', ':AiderAddModifiedFiles --mini --no-auto-commits<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>Am', ':AiderAddModifiedFiles<CR>', { noremap = true, silent = true })
 end
 
 
