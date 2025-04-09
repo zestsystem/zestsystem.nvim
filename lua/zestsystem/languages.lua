c1ebaadbb3f7535d81516372c11c55fe72001ab1
zestsystem <mk337337@gmail.com>
2025-04-09 11:20:19 -0700

    fix: fix syntax error and add lua formatting to conform

--- lua/zestsystem/languages.lua
+++ lua/zestsystem/languages.lua
@@ -79,9 +79,10 @@ local function init()
     optional = true,
     opts = {
         formatters_by_ft = {
+            lua = { "stylua" },
             javascript = { "prettierd", "prettier", { stop_after_first = true}},
             javascriptreact = { "prettierd", "prettier", { stop_after_first = true} },
-            typescript = { "prettierd", "prettier"< { stop_after_first = true} },
+            typescript = { "prettierd", "prettier", { stop_after_first = true} },
             typescriptreact = { "prettierd", "prettier", { stop_after_first = true} },
             html = { "prettierd", "prettier", { stop_after_first = true} },
             css = { "prettierd", "prettier", { stop_after_first = true} },
