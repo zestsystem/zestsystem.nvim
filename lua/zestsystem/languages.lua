75c05b334db988221981f026a06e9d55fad5f974
zestsystem <mk337337@gmail.com>
2025-04-09 11:16:30 -0700

    fix: set prettierd first

--- lua/zestsystem/languages.lua
+++ lua/zestsystem/languages.lua
@@ -79,12 +79,12 @@ local function init()
     optional = true,
     opts = {
         formatters_by_ft = {
-            javascript = { "prettier" },
-            javascriptreact = { "prettier" },
-            typescript = { "prettier" },
-            typescriptreact = { "prettier" },
-            html = { "prettier" },
-            css = { "prettier" },
+            javascript = { "prettierd", "prettier", { stop_after_first = true}},
+            javascriptreact = { "prettierd", "prettier", { stop_after_first = true} },
+            typescript = { "prettierd", "prettier"< { stop_after_first = true} },
+            typescriptreact = { "prettierd", "prettier", { stop_after_first = true} },
+            html = { "prettierd", "prettier", { stop_after_first = true} },
+            css = { "prettierd", "prettier", { stop_after_first = true} },
         },
     },
 })
