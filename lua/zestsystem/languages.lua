cefff7d0c596752dca8442d03e02f0364f46115d
zestsystem <mk337337@gmail.com>
2025-04-08 17:56:57 -0700

    feat: format on save

--- lua/zestsystem/languages.lua
+++ lua/zestsystem/languages.lua
@@ -91,6 +91,11 @@ local function init()
                     end,
                 },
             },
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
         },
     })
 
