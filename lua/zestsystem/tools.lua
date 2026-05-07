f22a060b2fbf2dc6228985006d419b4a259f064e
zestsystem <mk337337@gmail.com>
2026-05-07 17:13:55 +0900

    fix: remove diffview without startup hardening

--- lua/zestsystem/tools.lua
+++ lua/zestsystem/tools.lua
@@ -1,12 +1,6 @@
 local function harpoon()
-    local ok_mark, mark = pcall(require, "harpoon.mark")
-    local ok_ui, ui = pcall(require, "harpoon.ui")
-    if not (ok_mark and ok_ui) then
-        vim.schedule(function()
-            vim.notify("Skipping harpoon setup: plugin is unavailable", vim.log.levels.WARN, { title = "zestsystem.nvim" })
-        end)
-        return
-    end
+    local mark = require "harpoon.mark"
+    local ui = require "harpoon.ui"
 
     vim.keymap.set("n", "<leader><leader>g", mark.add_file)
     vim.keymap.set("n", "<leader><leader>f", ui.toggle_quick_menu)
